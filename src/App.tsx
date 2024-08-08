9c9c4c76cec9c1263775b8166251e82276ad1ab0
ydnamjs <ydnamjs@udel.edu>
2024-08-07 22:47:00 -0400

    Removed leftover code from personal website copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 
-export type PageType = "About" | "Portfolio" | "Other Interests" | "Contact + Links";
-
 function App() {
 	return <div className="app">sample text</div>;
 }
