ceb3370caa87083da72c2befe039828ba86f5a8e
ydnamjs <ydnamjs@udel.edu>
2024-08-10 06:20:57 -0400

    Added title and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import ImageUpload from "./pages/image-upload/ImageUpload";
 
 function App() {
-	return <div className="app">sample text</div>;
+	return (
+		<div className="app">
+			<div></div>
+			<ImageUpload />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/image-upload/ImageUpload.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ImageUpload.css";
+
+function ImageUpload() {
+	return (
+		<div className="image-upload">
+			<h1>Welcome To The D6 Counter Site!</h1>
+			<p>
+				Here you can upload images with six sided dice in them to have an AI model
+				automagically count them for you!
+			</p>
+		</div>
+	);
+}
+
+export default ImageUpload;
